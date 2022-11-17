975845b9576843c0f1bf8a9311c90f26c41486a1
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-11-17 11:53:56 +0000

    Mongo lab Included Mongoose

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import './seedData';
+import './db';
 
 dotenv.config();
 
