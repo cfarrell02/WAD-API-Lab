c2214258e06b647b0c2f69dfe7d40e74ef262738
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-12-01 12:27:45 +0000

    Topic 11 - Auth

--- reactApp/src/api/movie-api.js
+++ reactApp/src/api/movie-api.js
@@ -4,16 +4,16 @@ export const login = (username, password) => {
             'Content-Type': 'application/json'
         },
         method: 'POST',
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({username: username, password:password })
     }).then(res => res.json());
 };
 
 export const signup = (username, password) => {
-    return fetch('/api/users', {
+    return fetch('/api/users?action=register', {
         headers: {
             'Content-Type': 'application/json'
         },
         method: 'PUT',
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({username: username, password: password })
     }).then(res => res.json());
 }
